68509612866e9bcc226b9455873cae6bdbc56a06
michaelzeidan <mzeidan@radford.edu>
2019-03-01 16:47:06 -0500

    test

--- N3+h@X0r/core/src/com/mygdx/game/Player.java
+++ N3+h@X0r/core/src/com/mygdx/game/Player.java
@@ -5,4 +5,5 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Player extends Sprite {
     public static final int COLLECTABLES = 0;
     private boolean beatGame = false;
+    //test
 }
