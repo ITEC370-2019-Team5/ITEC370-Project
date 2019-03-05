4a7055b155705b203ab91a8b6d0799fdcc61556f
jtenaglia <48025608+jtenaglia@users.noreply.github.com>
2019-03-05 08:12:20 -0500

    Add files via upload
    Use this in Tiled

---
+++ N3+h@X0r/core/assets/PokemonTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Pokemon Tileset" tilewidth="16" tileheight="16" tilecount="12288" columns="192">
+ <image source="PokemonTileset.png" width="3072" height="1024"/>
+</tileset>
