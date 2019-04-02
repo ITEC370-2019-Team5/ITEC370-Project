1978de5fc14b792777df5d002073cfa4876c9b42
Michael Donaldson <mdonaldson@radford.edu>
2019-04-02 08:48:40 -0400

    Added the textScreen

---
+++ N3+h@X0r/core/src/com/mygdx/game/TextScreen.java
@@ -0,0 +1,94 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+import java.util.ArrayList;
+
+public class TextScreen implements Screen{
+    private NetworkingGame game; //Instance of the NetworkingGame class.
+    private OrthographicCamera camera; //The camera object.
+    private SpriteBatch batch; //Sprite batch object.
+    private Stage stage; //Stage to be displayed.
+    private Texture blackBox;
+    private Skin skin; //Skin object used for the TextField.
+    private boolean init; //Stops memory leaks.
+    String displayThis = "";
+    private Label label;
+
+    //Method to init the class.
+    public TextScreen(NetworkingGame game){
+        this.game = game;
+        camera = new OrthographicCamera();
+        init = false;
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+        batch.draw(blackBox, 50, 100);
+
+        batch.end();
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void show() {
+        if(init == false)
+        {
+            camera.setToOrtho(false, 600, 400);
+            batch = new SpriteBatch();
+            stage = new Stage();
+            blackBox = new Texture("core/assets/TextScreen.png");
+            init = true;
+
+            skin = new Skin(Gdx.files.internal("core/assets/clean-crispy/skin/clean-crispy-ui.json"));
+
+            label = new Label(displayThis, skin);
+            label.setFontScale(2);
+            label.setPosition(100, 125);
+            label.setSize(700, 200);
+
+            label.setText(displayThis);
+
+
+            stage.addActor(label);
+            Gdx.input.setInputProcessor(stage);
+
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() { dispose(); }
+    @Override
+    public void dispose () {
+    }
+    public void changeStr(String str)
+    {
+        displayThis = str;
+    }
+}
+
